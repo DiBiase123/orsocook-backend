e4c78197fda57c0f07f6be5e912609f629638507
Giuliano Di Biase <giulianofgfr84@gmail.com>
2026-07-09 19:37:23 +0200

    fix: rimosso export webDocuments da index

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,4 +2,3 @@ export { default as authController } from './auth/auth_controller';
 export * from './recipe/recipe_controller';
 export * from './category_controller';
 export * from './upload_controller';
-export { default as webDocumentsController } from './webDocuments/webDocuments_controller';
